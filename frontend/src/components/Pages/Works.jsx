b70c3b221ed0237e8f53b13c3d6bf62168863fa7
Rimpei-Qdai <hata.rimpei.319@s.kyushu-u.ac.jp>
2026-01-19 01:09:10 +0900

    worksページの内容を更新

--- frontend/src/components/Pages/Works.jsx
+++ frontend/src/components/Pages/Works.jsx
@@ -1,164 +1,217 @@
-import React from 'react'
-import '../../styles/works.css'
+import React from "react";
+import "../../styles/works.css";
 
 const Works = () => {
   return (
     <>
-    <div id="twoD-content" className='works'>
-      <h2 className="title">
-        <p>W</p>
-        <p>O</p>
-        <p>R</p>
-        <p>K</p>
-        <p>S</p>
-      </h2>
-      <a href='#' className="back-button" onClick={() => {
-        const sectionDOM = document.querySelector('#twoD-content')
-        sectionDOM.classList.remove('show')
-      }}>
-        <span></span>
-        <span></span>
-      </a>
+      <div id="twoD-content" className="works">
+        <h2 className="title">
+          <p>W</p>
+          <p>O</p>
+          <p>R</p>
+          <p>K</p>
+          <p>S</p>
+        </h2>
+        <a
+          href="#"
+          className="back-button"
+          onClick={() => {
+            const sectionDOM = document.querySelector("#twoD-content");
+            sectionDOM.classList.remove("show");
+          }}
+        >
+          <span></span>
+          <span></span>
+        </a>
 
-      <ul className="works-list">
-        <li className="works-item">
-          <div className="works-title">
-            Hosh!
-          </div>
-          <img src="/assets/imgs/Hosh.png" alt="" className="thumbnail" />
-          <p className="description">
-          「家族や恋人にプレゼントを選ぶのって難しい！」、「自分が本当に欲しいものを見つけるのって難しい！」そんな思いを解決るすために、「Hosh!」は誕生。今の自分の欲求を投稿することで、他ユーザーやLLMからほしい物をおすすめしてくれる。また、欲しいものリストを共有することで、プレゼント選びの参考にできる。「ほしい」をキーワードにユーザ間でのインタラクティブな環境を提供。
-          </p>
+        <ul className="works-list">
+          <li className="works-item">
+            <div className="works-title">Hosh!</div>
+            <img src="/assets/imgs/Hosh.png" alt="" className="thumbnail" />
+            <p className="description">
+              「家族や恋人にプレゼントを選ぶのって難しい！」、「自分が本当に欲しいものを見つけるのって難しい！」そんな思いを解決るすために、「Hosh!」は誕生。今の自分の欲求を投稿することで、他ユーザーやLLMからほしい物をおすすめしてくれる。また、欲しいものリストを共有することで、プレゼント選びの参考にできる。「ほしい」をキーワードにユーザ間でのインタラクティブな環境を提供。
+            </p>
 
-          <h3 className="used-techs">
-            <span className='pink'>■</span>使用技術
-          </h3>
-          <ul className="used-techs-list">
-            <li className='used-techs-item'>
-              php, MySQL, Google Apps Scripts, Matter.js
-            </li>
-          </ul>
-          <p className="description">
-          phpは、Laravelなどのフレームワークを一切使わすに実装することで、web技術を実践的に学ぶとともに、超軽量なアプリを実現。
-          </p>
+            <h3 className="used-techs">
+              <span className="pink">■</span>使用技術
+            </h3>
+            <ul className="used-techs-list">
+              <li className="used-techs-item">
+                php, MySQL, Google Apps Scripts, Matter.js
+              </li>
+            </ul>
+            <p className="description">
+              phpは、Laravelなどのフレームワークを一切使わすに実装することで、web技術を実践的に学ぶとともに、超軽量なアプリを実現。
+            </p>
 
-          <h3 className="used-techs">
-            ■実績
-          </h3>
+            <h3 className="used-techs">■実績</h3>
 
-          <ul className="achieves">
-          <li className="achieve">
+            <ul className="achieves">
+              <li className="achieve">
                 福岡未踏コンソーシアム 2023 Grow3期採択
-            </li>
-            <li className="achieve">
-                Engineer Driven Day 2023 決勝進出         
-            </li>
-            <li className="achieve">
-                技育博 2025 vol.6       
-            </li>
-           
-          </ul>
+              </li>
+              <li className="achieve">Engineer Driven Day 2023 決勝進出</li>
+              <li className="achieve">技育博 2025 vol.6</li>
+            </ul>
 
-          <a href='https://mitou-fukuoka.org/works/hosh/' target='_blank' className="more-button second">
-            <div className="text">
-            福岡未踏プロジェクトページ
-            </div>
-          </a>
-          <a href='https://protopedia.net/prototype/4674' target='_blank' className="more-button second">
-            <div className="text">
-            プロトぺディア
-            </div>
-          </a>
-          <a href='https://geek.supporterz.jp/geekpjt/' target='_blank' className="more-button">
-            <div className="text">
-            技育成プロジェクトとは
-            </div>
-          </a>
-        </li>
-        <li className="works-item">
-          <div className="works-title">
-            フリーランス
-          </div>
-          <img src="/assets/imgs/Wahiru-Ichino-portfolio.png" alt="" className="thumbnail" />
-          <p className="description">
-          フリーランスとして、システム開発の案件を受託。デザイナーのポートフォリオサイト開発や、学習塾の進捗管理システムの開発など、幅広く受注。案件と通して技術力を高めるとともに、営業からクローズまでを自身で行うことで、オールラウンドな経験を積む。
-          </p>
-          <a href='https://wahiru-f.com/' target='_blank' className="more-button">
-            <div className="text">
-            市野わひるさん HP
-            </div>
-          </a>
+            <a
+              href="https://mitou-fukuoka.org/works/hosh/"
+              target="_blank"
+              className="more-button second"
+            >
+              <div className="text">福岡未踏プロジェクトページ</div>
+            </a>
+            <a
+              href="https://protopedia.net/prototype/4674"
+              target="_blank"
+              className="more-button second"
+            >
+              <div className="text">プロトぺディア</div>
+            </a>
+            <a
+              href="https://geek.supporterz.jp/geekpjt/"
+              target="_blank"
+              className="more-button"
+            >
+              <div className="text">技育成プロジェクトとは</div>
+            </a>
+          </li>
+          <li className="works-item">
+            <div className="works-title">インターンシップ</div>
+            <img
+              src="/assets/imgs/internship.png"
+              alt=""
+              className="thumbnail no-border"
+            />
+            <p className="description">
+              2025年8月から11月の間に、4社様のインターンシップに参加。Laravel、TypeScriptを用いたフルスタック開発や、Ruby
+              on railsを社内公募システム開発、React
+              Nativeを用いたテレビシステムの開発、Java Spring
+              Bootのシステムにおける、Git Hub Spec
+              Kitを用いたテスト実装の効率化など、大規模なシステムでの実務経験を積む。
+            </p>
+            <a
+              href="https://beenos.com/"
+              target="_blank"
+              className="more-button second"
+            >
+              <div className="text">BEENOS株式会社</div>
+            </a>
+            <a
+              href="https://smarthr.jp/"
+              target="_blank"
+              className="more-button second"
+            >
+              <div className="text">株式会社SmartHR</div>
+            </a>
+            <a
+              href="https://www.play.jp/"
+              target="_blank"
+              className="more-button second"
+            >
+              <div className="text">株式会社PLAY</div>
+            </a>
+            <a
+              href="https://www.recruit.co.jp/"
+              target="_blank"
+              className="more-button"
+            >
+              <div className="text">株式会社リクルート</div>
+            </a>
 
-          <h3 className="used-techs">
-            ■使用技術
-          </h3>
-          <ul className="used-techs-list">
-            <li className='used-techs-item'>
-              php, Google Apps Scripts, wordpress
-            </li>
-          </ul>
+            <h3 className="used-techs">■使用技術</h3>
+            <ul className="used-techs-list">
+              <li className="used-techs-item">
+                <strong>■言語</strong>
+                <br />
+                &nbsp;&nbsp;- Laravel
+                <br />
+                &nbsp;&nbsp;- TypeScript
+                <br />
+                &nbsp;&nbsp;- Ruby on rails
+                <br />
+                &nbsp;&nbsp;- React Native
+                <br />
+                &nbsp;&nbsp;- Java Spring Boot
+                <br />
+                <strong>■アーキテクチャ</strong>
+                <br />
+                &nbsp;&nbsp;- SDD(Spec Driven Dvelopment)
+                <br />
+                &nbsp;&nbsp;- SDUI(ServerDriven UI)
+                <br />
+                &nbsp;&nbsp;- DDD(Domain Driven Development)
+                <br />
+                &nbsp;&nbsp;- Clean Architecture
+              </li>
+            </ul>
+          </li>
+          <li className="works-item">
+            <div className="works-title">フリーランス</div>
+            <img
+              src="/assets/imgs/Wahiru-Ichino-portfolio.png"
+              alt=""
+              className="thumbnail"
+            />
+            <p className="description">
+              フリーランスとして、システム開発の案件を受託。デザイナーのポートフォリオサイト開発や、学習塾の進捗管理システムの開発など、幅広く受注。案件と通して技術力を高めるとともに、営業からクローズまでを自身で行うことで、オールラウンドな経験を積む。
+            </p>
+            <a
+              href="https://wahiru-f.com/"
+              target="_blank"
+              className="more-button"
+            >
+              <div className="text">市野わひるさん HP</div>
+            </a>
 
-          
+            <h3 className="used-techs">■使用技術</h3>
+            <ul className="used-techs-list">
+              <li className="used-techs-item">
+                php, Google Apps Scripts, wordpress
+              </li>
+            </ul>
+          </li>
+          <li className="works-item">
+            <div className="works-title">株式会社RIWB</div>
+            <p className="description">
+              2022年6月、株式会社RIWBを設立し代表取締役社長に就任。自律したシンクタンクの実現を最終目標に掲げ、不動産検索サイトの共同開発やシステム案件受注を行う。福岡市の不動産データを分析し、賃貸相場に加え、長期的な賃貸と購入の場合の価格比較や周辺の教育環境を網羅的に検索できるアプリケーション開発を共同開発。同年12月、辞任。
+            </p>
+            <a href="https://riwb.org/" target="_blank" className="more-button">
+              <div className="text">株式会社RIWB HP</div>
+            </a>
 
-        </li>
-        <li className="works-item">
-          <div className="works-title">
-            株式会社RIWB
-          </div>
-          <p className="description">
-          2022年6月、株式会社RIWBを設立し代表取締役社長に就任。自律したシンクタンクの実現を最終目標に掲げ、不動産検索サイトの共同開発やシステム案件受注を行う。福岡市の不動産データを分析し、賃貸相場に加え、長期的な賃貸と購入の場合の価格比較や周辺の教育環境を網羅的に検索できるアプリケーション開発を共同開発。同年12月、辞任。
-          </p>
-          <a href='https://riwb.org/' target='_blank' className="more-button">
-            <div className="text">
-            株式会社RIWB HP
-            </div>
-          </a>
+            <h3 className="used-techs">■使用技術</h3>
+            <ul className="used-techs-list">
+              <li className="used-techs-item">python, php, wordpress</li>
+            </ul>
+          </li>
+          <li className="works-item">
+            <div className="works-title">その他</div>
+            <ul className="achieves">
+              <li className="achieve">Wework Student Ambassador</li>
+              <li className="achieve">ペンギンハック2024メンター</li>
+            </ul>
+            <a
+              href="https://hackspenguin.studio.site/"
+              target="_blank"
+              className="more-button second"
+            >
+              <div className="text">ペンギンハッカソン</div>
+            </a>
+            <a
+              href="https://www.instagram.com/wsa.japan/"
+              target="_blank"
+              className="more-button"
+            >
+              <div className="text">WSA</div>
+            </a>
+          </li>
+        </ul>
 
-
-          <h3 className="used-techs">
-            ■使用技術
-          </h3>
-          <ul className="used-techs-list">
-            {/* <li className='used-techs-item'>
-              <img src="/assets/imgs/php-logo.svg" alt="" className="techs-logo" />
-            </li>
-            <li className='used-techs-item'>
-              <img src="/assets/imgs/matter-js-logo.svg" alt="" className="techs-logo" />
-            </li>
-            <li className='used-techs-item'>
-              <img src="/assets/imgs/mysql-logo.png" alt="" className="techs-logo" />
-            </li>
-            <li className='used-techs-item'>
-              <img src="/assets/imgs/google-apps-script-logo.png" alt="" className="techs-logo" />
-            </li> */}
-            <li className='used-techs-item'>
-               python, php, wordpress
-            </li>
-          </ul>
-
-          
-        </li>
-        <li className="works-item">
-          <div className="works-title">
-            その他
-          </div>
-          <ul className="achieves">
-            <li className="achieve">
-            Wework Student Ambassador
-            </li>
-            <li className="achieve">
-            ペンギンハック2024メンター
-            </li>
-          </ul>
-          
-          
-        </li>
-      </ul>
-
-      <div className="footer">© 2025 Rimpei.H</div>
-    </div>
+        <div className="footer">© 2025 Rimpei.H</div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
